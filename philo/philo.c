01e2bcb539a07295bf186baa41a7232a2454b678
Albert Caballero Coll <alcaball@cbr10s3.42barcelona.com>
2023-11-19 17:32:35 +0100

    protected a few more after-death messages

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 12:13:56 by alcaball          #+#    #+#             */
-/*   Updated: 2023/11/18 20:01:18 by alcaball         ###   ########.fr       */
+/*   Updated: 2023/11/19 17:31:10 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,31 @@ void	*cycle(void *void_philo)
 			break ;
 		}
 		pthread_mutex_unlock(&philo->params->death_mtx);
-		pthread_mutex_lock(&philo->params->forks[philo->rfork_ix]);
-		printf("%lu %i has taken rfork\n", calc_reltime(philo, NOW), philo->num);
 
 		pthread_mutex_lock(&philo->params->death_mtx);
-		if (philo->params->death == DEAD){
-			pthread_mutex_unlock(&philo->params->death_mtx);
-			break ;
+		if (philo->params->death != DEAD)
+		{
+			pthread_mutex_lock(&philo->params->forks[philo->rfork_ix]);
+			printf("%lu %i has taken rfork\n", calc_reltime(philo, NOW), philo->num);
 		}
 		pthread_mutex_unlock(&philo->params->death_mtx);
 
-		pthread_mutex_lock(&philo->params->forks[philo->lfork_ix]);
-		printf("%lu %i has taken lfork\n", calc_reltime(philo, NOW), philo->num);
-		printf("%lu %i is eating\n", calc_reltime(philo, NOW), philo->num);
-		philo->status = EATING;
-		my_sleep(philo->params->tteat);
+		pthread_mutex_lock(&philo->params->death_mtx);
+		if (philo->params->death != DEAD)
+		{
+			pthread_mutex_lock(&philo->params->forks[philo->lfork_ix]);
+			printf("%lu %i has taken lfork\n", calc_reltime(philo, NOW), philo->num);
+			printf("%lu %i is eating\n", calc_reltime(philo, NOW), philo->num);
+			philo->status = EATING;
+			my_sleep(philo->params->tteat);	
+			pthread_mutex_lock(&philo->params->time_mtx);
+			philo->tlastmeal = calc_reltime(philo, NOW);
+			pthread_mutex_unlock(&philo->params->time_mtx);
+			pthread_mutex_unlock(&philo->params->forks[philo->rfork_ix]);
+			pthread_mutex_unlock(&philo->params->forks[philo->lfork_ix]);
+		}
+		pthread_mutex_unlock(&philo->params->death_mtx);
 
-		pthread_mutex_lock(&philo->params->time_mtx);
-		philo->tlastmeal = calc_reltime(philo, NOW);
-		pthread_mutex_unlock(&philo->params->time_mtx);
-
-		pthread_mutex_unlock(&philo->params->forks[philo->rfork_ix]);
-		pthread_mutex_unlock(&philo->params->forks[philo->lfork_ix]);
 		pthread_mutex_lock(&philo->params->death_mtx);
 		if (philo->params->death == DEAD)
 		{
