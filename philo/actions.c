7227e748e125aa6285c6a5462ac7922d05e4571b
Albert Caballero Coll <alcaball@cbr10s3.42barcelona.com>
2023-11-19 18:25:00 +0100

    workddsdfsdsf

--- philo/actions.c
+++ philo/actions.c
@@ -6,7 +6,7 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 19:26:32 by alcaball          #+#    #+#             */
-/*   Updated: 2023/11/18 14:15:27 by alcaball         ###   ########.fr       */
+/*   Updated: 2023/11/19 18:24:34 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 void	act_sleep(t_philos *philo)
 {
 	pthread_mutex_lock(&philo->params->death_mtx);
-	if (philo->params->death == DEAD){
-		pthread_mutex_unlock(&philo->params->death_mtx);
-		return ;
+	if (philo->params->death != DEAD){
+		pthread_mutex_lock(&philo->params->msg_mtx);
+		printf("%lu %i is sleeping\n", my_time() - philo->params->starttime, philo->num);
+		pthread_mutex_unlock(&philo->params->msg_mtx);
 	}
 	pthread_mutex_unlock(&philo->params->death_mtx);
-	pthread_mutex_lock(&philo->params->msg_mtx);
-	printf("%lu %i is sleeping\n", my_time() - philo->params->starttime, philo->num);
-	pthread_mutex_unlock(&philo->params->msg_mtx);
 	philo->status = SLEEPING;
 	my_sleep(philo->params->ttsleep);
 }
@@ -35,13 +33,11 @@ void	act_eat(t_philos *philo)
 void	act_think(t_philos *philo)
 {
 	pthread_mutex_lock(&philo->params->death_mtx);
-	if (philo->params->death == DEAD){
-		pthread_mutex_unlock(&philo->params->death_mtx);
-		return ;
+	if (philo->params->death != DEAD){
+		pthread_mutex_lock(&philo->params->msg_mtx);
+		printf("%lu %i is thinking\n", my_time() - philo->params->starttime, philo->num);
+		pthread_mutex_unlock(&philo->params->msg_mtx);
 	}
 	pthread_mutex_unlock(&philo->params->death_mtx);
-	pthread_mutex_lock(&philo->params->msg_mtx);
-	printf("%lu %i is thinking\n", my_time() - philo->params->starttime, philo->num);
-	pthread_mutex_unlock(&philo->params->msg_mtx);
 	philo->status = THINKING;
 }
