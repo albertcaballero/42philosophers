1735924a3e4a0bee36f97caeeb02cd9f3d0c97d9
Albert Caballero Coll <alcaball@car9s2.42barcelona.com>
2023-11-21 12:14:13 +0100

    some changes

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: alcaball <alcaball@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 12:13:56 by alcaball          #+#    #+#             */
-/*   Updated: 2023/11/19 18:50:25 by alcaball         ###   ########.fr       */
+/*   Updated: 2023/11/21 12:13:39 by alcaball         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,14 @@ void	*cycle(void *void_philo)
 		}
 		pthread_mutex_unlock(&philo->params->death_mtx);
 
-		pthread_mutex_lock(&philo->params->death_mtx); //NEXT STEP ES PASAR ESTO A ACT_EAT
+		pthread_mutex_lock(&philo->params->death_mtx);
 		if (philo->params->death != DEAD)
-		{
+		{ //cuando se quedan esperando a un lock no se estan muriendo, & demiurge cannot see it cuz deat_mtx is locked :)
 			pthread_mutex_lock(&philo->params->forks[philo->lfork_ix]);
 			printf("%lu %i has taken lfork\n", calc_reltime(philo, NOW), philo->num);
 			printf("%lu %i is eating\n", calc_reltime(philo, NOW), philo->num);
 			philo->status = EATING;
-			my_sleep(philo->params->tteat);	
+			my_sleep(philo->params->tteat);
 			pthread_mutex_lock(&philo->params->time_mtx);
 			philo->tlastmeal = calc_reltime(philo, NOW);
 			pthread_mutex_unlock(&philo->params->time_mtx);
@@ -74,10 +74,17 @@ void	demiurge(t_params *params)
 		if (i > params->num - 1)
 			i = 0;
 		pthread_mutex_lock(&params->death_mtx);
+		printf("I HAVE THE MUTEEEEX");
 		check_dead(&params->philos[i]);
 		if (params->death == DEAD)
-			while (i++ < params->num - 1)
+		{
+			i = 0;
+			while (i++ < params->num)
+			{
 				params->philos[i].status = DEAD;
+				pthread_mutex_destroy(&params->forks[i]);
+			}
+		}
 		pthread_mutex_unlock(&params->death_mtx);
 		if (params->eatmax > 0)
 		{
